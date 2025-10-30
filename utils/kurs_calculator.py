4ffabfb6e487629dab851629f92492e59281e7ce
Perman-developer <permanshamuratov@gmail.com>
2025-10-30 11:53:39 +0500

    Update kurs_calculator.py

--- utils/kurs_calculator.py
+++ utils/kurs_calculator.py
@@ -2,6 +2,8 @@ from config import kurs_rub, kurs_usd, foiz
 
 def CALCULATOR(api_id, rate, quantity):
     if api_id == 1:
-       return round(quantity * kurs_rub * rate * foiz / 1000, 2)
+        return round(quantity * kurs_rub * rate * foiz / 1000, 2)
+    elif api_id == 4:
+        return round(quantity * rate * foiz / 1000, 2)
     else:
-       return round(quantity * kurs_usd * rate * foiz / 1000, 2)
+        return round(quantity * kurs_usd * rate * foiz / 1000, 2)
